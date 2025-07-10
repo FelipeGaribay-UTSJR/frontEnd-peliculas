6ed3259b9856a376bb1db264e93b4681e473f1ac
FelipeGaribay-UTSJR <felipegu.ti22@utsjr.edu.mx>
2025-07-09 19:31:37 -0600

    routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -10,6 +10,11 @@ export const serverRoutes: ServerRoute[] = [
     renderMode: RenderMode.Prerender
   },
   {
+    path: 'editar-pelicula/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => []
+  },
+  {
     path: '',
     renderMode: RenderMode.Prerender
   },
